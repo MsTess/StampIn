ce40b28ad305d9025f2134ccf810931d843dddeb
Ben Baldieri <ben.baldieri@gmail.com>
2021-07-15 21:51:35 +0800

    added filter if/else to embassies index method

--- app/controllers/embassies_controller.rb
+++ app/controllers/embassies_controller.rb
@@ -1,15 +1,19 @@
 class EmbassiesController < ApplicationController
-  def index
-    @embassies = Embassy.all
+def index
+  if params[:origin_country]
+    @embassies = Embassy.where(:origin_country => params[:origin_country])
+  else
+    false
   end
+end
 
   def show
     @embassy = Embassy.find(params[:id])
 
     @markers =
     {
-        lat: @embassy.latitude,
-        lng: @embassy.longitude
+      lat: @embassy.latitude,
+      lng: @embassy.longitude,
     }
   end
 end
