2fd279641d4dc0bb8a0e47fe0eac1a16cb5568bc
Ben Baldieri <ben.baldieri@gmail.com>
2021-07-20 22:32:05 +0800

    countries page aligned

--- app/models/country.rb
+++ app/models/country.rb
@@ -11,4 +11,12 @@ class Country < ApplicationRecord
   #          :demonym,
   #          :latlng,
   #          :callingCodes, presence: true
+
+    def background_image
+    pixabay_api_key = '21611264-538b8cdc7adae5ce01738c67b'
+    name = @country.name
+    url = "https://pixabay.com/api/?key=#{pixabay_api_key}&q=#{name}&image_type=photo&pretty=true"
+    parsed_response = JSON.parse(RestClient.get(url))
+    parsed_response["hits"].present? ? parsed_response["hits"][0]["largeImageURL"] : ActionController::Base.helpers.asset_path("anton-8eG73NjiHao-unsplash.jpg")
+  end
 end
