f6a133f9dd3dab8e319b27990f203801f18db2db
Ben Baldieri <ben.baldieri@gmail.com>
2021-07-27 14:00:29 +0100

    changes to seed and embassy show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,8 @@ countries.each {|country|
 
     c_lowercase = country.name.downcase.gsub(" ", "-").gsub(/[^\x00-\x7F]/, 'a')
 
-  postman_url = "https://onlinevisa.com/page-data/embassies/#{c_lowercase}/page-data.json"
+  # postman_url = "https://onlinevisa.com/page-data/embassies/#{c_lowercase}/page-data.json"
+  postman_url = "https://onlinevisa.com/page-data/embassies/united-kingdom/page-data.json"
 
   # Embassy.destroy_all
   begin
